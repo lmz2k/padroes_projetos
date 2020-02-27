932297fae9fb119ed4dc56323c487f1543a174f9
lmz2k <gabriel1997lima@gmail.com>
2020-02-27 15:55:25 -0300

    end

--- edapter_exercise/src/com/company/Adapter.java
+++ edapter_exercise/src/com/company/Adapter.java
@@ -5,7 +5,8 @@ public class Adapter extends Tomada_tres_pinos implements Conector_2_pinos{
 
 
     public Adapter( boolean pNeutro, boolean pFase) {
-        super(false, pNeutro, pFase);
+//        super(false, pNeutro, pFase);
+        super(true, pNeutro, pFase);
 
     }
 
@@ -13,7 +14,7 @@ public class Adapter extends Tomada_tres_pinos implements Conector_2_pinos{
     @Override
     public void conectar() {
 
-        super.setpTerra(true);
+//        super.setpTerra(true);
         super.conectarComEnergia();
 
 
